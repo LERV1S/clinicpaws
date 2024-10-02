06bd9a6ce32170c8464eea090295335e2c26b3bc
LERV1S <luis.rivera2887@alumnos.udg.mx>
2024-10-01 20:08:04 -0600

    web route

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PrescriptionController;
 use App\Http\Controllers\InvoiceController;
 
 // Rutas generales
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
