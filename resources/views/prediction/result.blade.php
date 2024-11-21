aecdaf9237214cac7f4d28e7c0343ebc74e45c9d
LERV1S <luis.rivera2887@alumnos.udg.mx>
2024-11-20 18:30:42 -0600

    ml v2

--- resources/views/prediction/result.blade.php
+++ resources/views/prediction/result.blade.php
@@ -3,7 +3,14 @@
 @section('content')
     <div class="container">
         <h1>Resultado de la predicción</h1>
-        <p>{{ $interpretation }}</p>
-        <a href="{{ route('predict') }}" class="btn btn-secondary">Volver</a>
+
+        {{-- Mostrar el mensaje interpretado basado en la predicción --}}
+        @if($prediction['prediction'] == 1)
+            <p>Alto riesgo para la salud. Se recomienda acudir al veterinario lo más pronto posible.</p>
+        @elseif($prediction['prediction'] == 0)
+            <p>Bajo riesgo para la salud. Monitorea al animal y acude al veterinario si los síntomas persisten.</p>
+        @else
+            <p>Predicción desconocida.</p>
+        @endif
     </div>
 @endsection
