2fe012bd6e1ecab2256168101f78c2249aa09f59
LERV1S <luis.rivera2887@alumnos.udg.mx>
2024-11-20 18:44:43 -0600

    correccion ml

--- resources/views/prediction/result.blade.php
+++ resources/views/prediction/result.blade.php
@@ -4,13 +4,7 @@
     <div class="container">
         <h1>Resultado de la predicción</h1>
 
-        {{-- Mostrar el mensaje interpretado basado en la predicción --}}
-        @if($prediction['prediction'] == 1)
-            <p>Alto riesgo para la salud. Se recomienda acudir al veterinario lo más pronto posible.</p>
-        @elseif($prediction['prediction'] == 0)
-            <p>Bajo riesgo para la salud. Monitorea al animal y acude al veterinario si los síntomas persisten.</p>
-        @else
-            <p>Predicción desconocida.</p>
-        @endif
+        <p>{{ $interpretation }}</p>
     </div>
 @endsection
+
