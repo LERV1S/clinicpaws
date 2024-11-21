900c73832fa2709c5903bd2199957affd13e3bee
LERV1S <luis.rivera2887@alumnos.udg.mx>
2024-11-20 19:39:20 -0600

    El chido 1.0 ML

--- resources/views/prediction/result.blade.php
+++ resources/views/prediction/result.blade.php
@@ -6,8 +6,5 @@
 
         {{-- Mostrar el mensaje interpretado basado en la predicción --}}
         <p>{{ $interpretation }}</p>
-
-        {{-- Mostrar el valor de la predicción para referencia (opcional) --}}
-        <p><strong>Predicción numérica:</strong> {{ $prediction }}</p>
     </div>
 @endsection
